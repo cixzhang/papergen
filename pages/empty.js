be28d537c5a26681159c954a5c2872c76b4a68a3
Cindy Zhang <cixzhang@gmail.com>
2018-05-13 19:35:26 -0700

    calling electron from cli

--- pages/empty.js
+++ pages/empty.js
@@ -4,9 +4,7 @@ const Page = require('../lib/page');
 const empty = new Page({
   name: 'Empty',
   description: 'Renders an empty page.',
-  render: (renderer) => {
-    renderer.createPage();
-  },
+  render: () => {},
 });
 
 module.exports = empty;
