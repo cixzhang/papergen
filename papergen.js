95e22788a4fdd74560d055e3f29dff507dfe46c7
Cindy Zhang <cixzhang@gmail.com>
2018-05-13 22:52:01 -0700

    moving main logic to papergen.js

--- papergen.js
+++ papergen.js
@@ -1,13 +1,31 @@
 #!/usr/bin/env node
 // Papergen
 
+const path = require('path');
 const cli = require('./lib/cli');
-const papergen = {
-  Page: require('./lib/page'),
-};
+
+const { spawn } = require('child_process');
+
+function papergen(pageConfigs) {
+  // build js with each page
+  // start electron with built files
+  // render(mod, options)
+  //   .catch(e => console.error(e))
+  //   .then(buf => process.stdout.write(buf));
+  const electron = path.resolve(__dirname, 'node_modules', '.bin', 'electron');
+  const main = path.resolve(__dirname, 'lib', 'main.js');
+  const child = spawn(electron, [main]);
+
+  child.stdout.on('data', (data) => { console.log('Electron: ', data); });
+  child.stderr.on('data', (data) => { console.error('Electron: ', data); });
+  child.on('close', (code) => { console.log(`Electron process closed with ${code}`); });
+}
+
+papergen.Page = require('./lib/page');
 
 if (require.main === module) {
-  cli(process.argv);
+  const {pageConfigs} = cli(process.argv);
+  return papergen(pageConfigs);
 }
 
 module.exports = papergen;
