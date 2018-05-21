2591845ad9b738c5b84664c6655e6a0663f94205
Cindy Zhang <cixzhang@gmail.com>
2018-05-20 22:20:03 -0700

    rendering grid

--- pages/grid.js
+++ pages/grid.js
@@ -1,5 +1,6 @@
 
 const Page = require('../lib/page');
+const PageSizes = require('../lib/pageSizes');
 
 const grid = new Page({
   name: 'Grid',
@@ -17,7 +18,36 @@ const grid = new Page({
     },
   ],
   render: (el, options) => {
-    console.log(options);
+    const rect = el.getBoundingClientRect();
+    const {width, height} = rect;
+    const ns = 'http://www.w3.org/2000/svg';
+    const svg = document.createElementNS(ns, 'svg');
+    const gridSize = PageSizes.toPixelsFromMM([5], options.ppi)[0];
+    // TODO: debug this
+    for (let x = gridSize; x < width; x += gridSize) {
+      for (let y = gridSize; y < height; y += gridSize) {
+        const hline = document.createElementNS(ns, 'line');
+        hline.setAttributeNS(ns, 'x1', 0);
+        hline.setAttributeNS(ns, 'x2', width);
+        hline.setAttributeNS(ns, 'y1', y);
+        hline.setAttributeNS(ns, 'y2', y);
+        hline.setAttributeNS(ns, 'stroke', 'black');
+
+        const vline = document.createElement('line');
+        vline.setAttributeNS(ns, 'x1', x);
+        vline.setAttributeNS(ns, 'x2', x);
+        vline.setAttributeNS(ns, 'y1', 0);
+        vline.setAttributeNS(ns, 'y2', height);
+        vline.setAttributeNS(ns, 'stroke', 'black');
+
+        svg.appendChild(hline);
+        svg.appendChild(vline);
+      }
+    }
+
+    svg.style.width = '100%';
+    svg.style.height = '100%';
+    el.appendChild(svg);
   },
 });
 
