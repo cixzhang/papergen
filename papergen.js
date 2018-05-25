dcfa6593590b867df5acf5e2f677792a981b0fd2
Cindy Zhang <cixzhang@gmail.com>
2018-05-24 23:12:41 -0700

    functioning pdf gen

--- papergen.js
+++ papergen.js
@@ -8,21 +8,21 @@ const build = require('./lib/build');
 const { spawn } = require('child_process');
 
 function papergen(pageConfigs, rendererOptions) {
-  // build js with each page
-  // start electron with built files
-  // render(mod, options)
-  //   .catch(e => console.error(e))
-  //   .then(buf => process.stdout.write(buf));
-  console.log(pageConfigs, rendererOptions);
   build(pageConfigs, rendererOptions);
 
   const electron = path.resolve(__dirname, 'node_modules', '.bin', 'electron');
   const main = path.resolve(__dirname, 'lib', 'main.js');
   const child = spawn(electron, [main]);
 
-  child.stdout.on('data', (data) => { console.log('Electron: ', data); });
-  child.stderr.on('data', (data) => { console.error('Electron: ', data); });
-  child.on('close', (code) => { console.log(`Electron process closed with ${code}`); });
+  child.stdout.on('data', (data) => {
+    console.log('Electron: ', data.toString());
+  });
+  child.stderr.on('data', (data) => {
+    console.error('Electron: ', data.toString());
+  });
+  child.on('close', (code) => {
+    console.log(`Electron process closed with ${code}`);
+  });
 }
 
 papergen.Page = require('./lib/page');
